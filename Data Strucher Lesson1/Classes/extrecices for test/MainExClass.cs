e4f7318c4e9bbe260fb3649081921776f016bdc6
shay shalom izhak <alonizhakalonizhak@gmail.com>
2024-07-17 19:26:24 +0300

    added some new another changes

--- Data Strucher Lesson1/Classes/extrecices for test/MainExClass.cs
+++ Data Strucher Lesson1/Classes/extrecices for test/MainExClass.cs
@@ -35,6 +35,157 @@ namespace Data_Strucher_Lesson1.Classes.extrecices_for_test
             }
             return false;
         }
+        //EX3
+        public static void EnterLast(IntNode lst, int num)
+        {
+            IntNode pos = lst;
+            while(pos.HasNext())
+            {
+                pos = pos.GetNext();
+            }
+            pos.SetValue(num);
+        }
+        //EX4
+        public static void EnterSecond(IntNode lst, int num)
+        {
+            int count = 0;
+            IntNode pos = lst;
+            while(pos != null)
+            {
+                count++;
+                if(count == 2)
+                {
+                    pos.SetValue(num);
+                }
+                pos = pos.GetNext();
+            }
+        }
+        //EX5
+        public static int Size(IntNode lst)
+        {
+            int count = 0;
+            IntNode pos = lst;
+            while(pos != null)
+            {
+                count++;
+                pos = pos.GetNext();
+            }
+            return count;
+        }
+        //EX6
+        public static int HowMany(IntNode lst, int num)
+        {
+            int count = 0;
+            IntNode pos = lst;
+            while(pos != null)
+            {
+                if (pos.GetValue() == num)
+                    count++;
+
+                pos = pos.GetNext();
+            }
+            return count;
+        }
+        //EX7
+        public static bool InOrder(IntNode lst)
+        {
+            IntNode pos = lst;
+            while (pos.HasNext())
+            {
+                if(pos.GetValue() > pos.GetNext().GetValue())
+                    return false;
+
+                pos = pos.GetNext();
+            }
+            return true;
+        }
+        //EX8
+        public static int SumOdd(IntNode lst)
+        {
+            IntNode pos = lst;
+            int count = 0;
+            int sum = 0;
+            while(pos != null)
+            {
+                count++;
+                if (count % 2 != 0)
+                    sum += pos.GetValue();
+
+                pos = pos.GetNext();
+            }
+            return sum;
+        }
+        //EX9
+        public static void EnterInOrder(IntNode lst, int num)
+        {
+            IntNode newNode = new IntNode(num);
+
+            if (lst == null || lst.GetValue() >= num)
+            {
+                newNode.setNext(lst);
+                lst = newNode;
+                return;
+            }
+
+
+            IntNode current = lst;
+            while (current.HasNext() && current.GetNext().GetValue() < num)
+            {
+                current = current.GetNext();
+            }
+
+            newNode.setNext(current.GetNext());
+            current.setNext(newNode);
+        }
+        //EX10
+        public static bool IsSerial(IntNode lst)
+        {
+            if(lst == null || !lst.HasNext())
+                return true;
+
+            IntNode current = lst;
+            int diff = current.GetNext().GetValue() - current.GetValue();
+            while (current.HasNext())
+            {
+                if (current.GetNext().GetValue() - current.GetValue() != diff)
+                    return false;
+
+                current = current.GetNext();
+            }
+            return true;
+        }
+        //EX11
+        public static IntNode RemovePos(IntNode lst, int num)
+        {
+            IntNode pos = lst;
+            int count = 0;
+            while (pos != null)
+            {
+                count++;
+                if (count == num)
+                {
+                    pos.setNext(pos.GetNext().GetNext());
+                }
+                pos = pos.GetNext();
+            }
+            return pos;
+        }
+        //EX12
+        public static int ReturnAtPos(IntNode lst, int step)
+        {
+            IntNode pos = lst;
+            int count = 0;
+            int val = 0; ;
+            while (pos != null)
+            {
+                count++;
+                if(count == step)
+                {
+                    val = pos.GetValue();
+                }
+            }
+            return val;
+        }
     }
     public class RunMainExClass
     {
@@ -53,6 +204,23 @@ namespace Data_Strucher_Lesson1.Classes.extrecices_for_test
             Console.WriteLine($"the sum of the list is : {sum}");
             int num = 1;
             Console.WriteLine($"is the number {num} in the list? {MainExClass.IsExsist(n1,num)}");
+            Console.WriteLine("\nthe original list :");
+            IntNode.print(n1);
+            //Console.WriteLine("the list after entering 30 in the second place!");
+            //MainExClass.EnterSecond(n1, 30);
+            //IntNode.print(n1);
+            Console.WriteLine($"the size of the list is : {MainExClass.Size(n1)}");
+            Console.WriteLine($"the amount of times the the number 14 is of the list is {MainExClass.HowMany(n1,14)}");
+            Console.WriteLine($"is the list in order ? {MainExClass.InOrder(n1)}");
+            Console.WriteLine($"the sum in the odd indexs is {MainExClass.SumOdd(n1)}");
+            //MainExClass.EnterInOrder(n1, 45);
+            //IntNode.print(n1);
+            Console.WriteLine($"is the list serial? {MainExClass.IsSerial(n1)}");
+            Console.WriteLine("the list after deleteing 54 :");
+            //MainExClass.RemovePos(n1,5);
+            //IntNode.print(n1);
+
+
         }
     }
 }
