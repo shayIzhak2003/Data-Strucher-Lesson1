eed8bc9037fe1b2db188fe048e88628c3e8d8b3e
shay shalom izhak <alonizhakalonizhak@gmail.com>
2024-09-04 20:52:24 +0300

    finished ring app

--- Data Strucher Lesson1/Classes/stackStrucher/stack Objects/Ring App/Pole.cs
+++ Data Strucher Lesson1/Classes/stackStrucher/stack Objects/Ring App/Pole.cs
@@ -19,15 +19,40 @@ namespace Data_Strucher_Lesson1.Classes.stackStrucher.stack_Objects.Ring_App
         }
         public Ring Remove(Ring r)
         {
-             return this.po.Pop();
+            return this.po.Pop();
         }
         public bool IsEmpty()
         {
-          return po.IsEmpty();
+            return po.IsEmpty();
         }
         public void Sort()
         {
+            Stack<Ring> stackL = new Stack<Ring>();
+            Stack<Ring> stackS = new Stack<Ring>();
+            while (!this.po.IsEmpty())
+            {
+                Ring ring = this.po.Pop();
+                if (ring.GetSize() == "L")
+                {
+                    stackL.Push(ring);
+                }
+                else
+                {
+                    stackS.Push(ring);
+                }
+            }
 
+            while (!stackL.IsEmpty())
+            {
+                Ring currentRing = stackL.Pop();
+                Add(currentRing);
+            }
+
+            while (!stackS.IsEmpty())
+            {
+                Ring currentRing = stackS.Pop();
+                Add(currentRing);
+            }
         }
 
     }
--- Data Strucher Lesson1/Classes/stackStrucher/stack Objects/Ring App/RunRingApp.cs
+++ Data Strucher Lesson1/Classes/stackStrucher/stack Objects/Ring App/RunRingApp.cs
@@ -10,7 +10,43 @@ namespace Data_Strucher_Lesson1.Classes.stackStrucher.stack_Objects.Ring_App
     {
         public static void DemoMain()
         {
-            Console.WriteLine("hello,world!");
+            // Create a Pole object
+            Pole pole = new Pole();
+
+            // Add rings to the pole
+            pole.Add(new Ring("L", 1));
+            pole.Add(new Ring("S", 2));
+            pole.Add(new Ring("L", 3));
+            pole.Add(new Ring("S", 4));
+
+            // Display the pole's content before sorting
+            Console.WriteLine("Pole before sorting:");
+            PrintPole(pole);
+
+            // Sort the rings on the pole
+            pole.Sort();
+
+            // Display the pole's content after sorting
+            Console.WriteLine("Pole after sorting:");
+            PrintPole(pole);
+        }
+
+        static void PrintPole(Pole pole)
+        {
+            // Since we can't directly access the stack, we'll use a temporary stack to display the contents
+            Stack<Ring> tempStack = new Stack<Ring>();
+            while (!pole.IsEmpty())
+            {
+                Ring ring = pole.Remove(null);  // Use a null value because the parameter is not necessary
+                tempStack.Push(ring);
+                Console.WriteLine($"Ring size: {ring.GetSize()}, color: {ring.GetColor()}");
+            }
+
+            // Restore the rings back to the original pole
+            while (!tempStack.IsEmpty())
+            {
+                pole.Add(tempStack.Pop());
+            }
         }
     }
 }
