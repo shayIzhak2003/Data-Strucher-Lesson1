1721982efe3e5b68f4b138d5268098e2427aadc8
shay shalom izhak <alonizhakalonizhak@gmail.com>
2024-12-23 09:50:10 +0200

    added a few new changes

--- Data Strucher Lesson1/Classes/Binray-tree/Lessons/TreeLs2.cs
+++ Data Strucher Lesson1/Classes/Binray-tree/Lessons/TreeLs2.cs
@@ -15,9 +15,9 @@ public class TreeLs2
             // If the symbol is a dot (.), move to the left child node
             if (symbol == '.')
             {
-                if (currentNode.HasLeft())
+                if (currentNode.HasRight())
                 {
-                    currentNode = currentNode.GetLeft();
+                    currentNode = currentNode.GetRight();
                 }
                 else
                 {
@@ -27,9 +27,9 @@ public class TreeLs2
             // If the symbol is a dash (-), move to the right child node
             else if (symbol == '-')
             {
-                if (currentNode.HasRight())
+                if (currentNode.HasLeft())
                 {
-                    currentNode = currentNode.GetRight();
+                    currentNode = currentNode.GetLeft();
                 }
                 else
                 {
@@ -62,7 +62,48 @@ public class TreeLs2
         // עץ שטוח אם מספר העלים הוא לפחות חצי ממספר הצמתים
         return leafCount >= (treeCount / 2);
     }
+    //EX3
+    public static bool IsYoungTree(BinNode<int> root)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+        if (BasicFunctioms.IsLeaf(root))
+        {
+            return true;
+        }
+        // בדיקה האם השורש קטן משני תתי-העצים
+        if (root.GetValue() >= root.GetLeft().GetValue() || root.GetValue() >= root.GetRight().GetValue())
+        {
+            return false;
+        }
 
+        // בדיקה האם תת-העץ הימני גדול מתת-העץ השמאלי
+        if (root.GetRight().GetValue() <= root.GetLeft().GetValue())
+        {
+            return false;
+        }
+        return IsYoungTree(root.GetLeft()) && IsYoungTree(root.GetRight());
+    }
+    //EX4
+    //public static bool IsSigmaTree(BinNode<int> root)
+    //{
+    //    if (root == null)
+    //    {
+    //        return true;
+    //    }
+
+    //    if (BasicFunctioms.IsLeaf(root))
+    //    {
+    //        return true; // עלה נחשב כעץ סיגמה
+    //    }
+
+    //    if (!root.HasLeft() || !root.HasRight())
+    //    {
+    //        return false; // אם חסר בן אחד, זה לא עץ סיגמה
+    //    }
+    //}
 }
 
 public class RunTreeLs2
@@ -70,43 +111,46 @@ public class RunTreeLs2
     // Build the Morse Tree with the correct structure.
     public static BinNode<char> BuildMorseTree()
     {
-        // Root node represents the start of the Morse code tree.
-        var root = new BinNode<char>(' ');
+        // Root node
+        var Root = new BinNode<char>(' ');
 
-        // Level 1: Root and first level of Morse characters
-        root.SetLeft(new BinNode<char>('E'));  // Dot for 'E'
-        root.SetRight(new BinNode<char>('T')); // Dash for 'T'
+        // Level 1
+        Root.SetLeft(new BinNode<char>('T'));
+        Root.SetRight(new BinNode<char>('E'));
 
-        // Level 2: Second level of Morse characters
-        root.GetLeft().SetLeft(new BinNode<char>('I'));  // Dot for 'I'
-        root.GetLeft().SetRight(new BinNode<char>('A')); // Dot for 'A'
-        root.GetRight().SetLeft(new BinNode<char>('N')); // Dash for 'N'
-        root.GetRight().SetRight(new BinNode<char>('M')); // Dash for 'M'
+        // Left subtree of T
+        Root.GetLeft().SetLeft(new BinNode<char>('M'));
+        Root.GetLeft().SetRight(new BinNode<char>('N'));
+        Root.GetLeft().GetLeft().SetLeft(new BinNode<char>('O'));
+        Root.GetLeft().GetLeft().SetRight(new BinNode<char>('G'));
+        Root.GetLeft().GetLeft().GetRight().SetLeft(new BinNode<char>('Q'));
+        Root.GetLeft().GetLeft().GetRight().SetRight(new BinNode<char>('Z'));
+        Root.GetLeft().GetRight().SetLeft(new BinNode<char>('K'));
+        Root.GetLeft().GetRight().SetRight(new BinNode<char>('D'));
+        Root.GetLeft().GetRight().GetLeft().SetLeft(new BinNode<char>('Y'));
+        Root.GetLeft().GetRight().GetLeft().SetRight(new BinNode<char>('C'));
+        Root.GetLeft().GetRight().GetRight().SetLeft(new BinNode<char>('X'));
+        Root.GetLeft().GetRight().GetRight().SetRight(new BinNode<char>('B'));
 
-        // Level 3: Third level of Morse characters
-        root.GetLeft().GetLeft().SetLeft(new BinNode<char>('S'));  // Dot for 'S'
-        root.GetLeft().GetLeft().SetRight(new BinNode<char>('U')); // Dot for 'U'
-        root.GetLeft().GetRight().SetLeft(new BinNode<char>('R')); // Dot for 'R'
-        root.GetLeft().GetRight().SetRight(new BinNode<char>('W')); // Dot for 'W'
-        root.GetRight().GetLeft().SetLeft(new BinNode<char>('K')); // Dash for 'K'
-        root.GetRight().GetLeft().SetRight(new BinNode<char>('D')); // Dash for 'D'
-        root.GetRight().GetRight().SetLeft(new BinNode<char>('O')); // Dash for 'O'
-        root.GetRight().GetRight().SetRight(new BinNode<char>('G')); // Dash for 'G'
+        // Right subtree of E
+        Root.GetRight().SetLeft(new BinNode<char>('A'));
+        Root.GetRight().SetRight(new BinNode<char>('I'));
+        Root.GetRight().GetLeft().SetLeft(new BinNode<char>('W'));
+        Root.GetRight().GetLeft().SetRight(new BinNode<char>('R'));
+        Root.GetRight().GetLeft().GetLeft().SetLeft(new BinNode<char>('J'));
+        Root.GetRight().GetLeft().GetLeft().SetRight(new BinNode<char>('P'));
+        Root.GetRight().GetLeft().GetRight().SetLeft(new BinNode<char>('L'));
+        Root.GetRight().GetLeft().GetRight().SetRight(new BinNode<char>('8'));
+        Root.GetRight().GetRight().SetLeft(new BinNode<char>('U'));
+        Root.GetRight().GetRight().SetRight(new BinNode<char>('S'));
+        Root.GetRight().GetRight().GetLeft().SetLeft(new BinNode<char>('V'));
+        Root.GetRight().GetRight().GetLeft().SetRight(new BinNode<char>('F'));
+        Root.GetRight().GetRight().GetRight().SetLeft(new BinNode<char>('0'));
+        Root.GetRight().GetRight().GetRight().SetRight(new BinNode<char>('H'));
 
-        // Level 4: Fourth level of Morse characters
-        root.GetLeft().GetLeft().GetLeft().SetLeft(new BinNode<char>('H'));  // Dot for 'H'
-        root.GetLeft().GetLeft().GetLeft().SetRight(new BinNode<char>('V')); // Dot for 'V'
-        root.GetLeft().GetLeft().GetRight().SetLeft(new BinNode<char>('F')); // Dot for 'F'
-        root.GetLeft().GetLeft().GetRight().SetRight(new BinNode<char>('L')); // Dot for 'L'
-        root.GetLeft().GetRight().GetLeft().SetLeft(new BinNode<char>('P')); // Dot for 'P'
-        root.GetLeft().GetRight().GetLeft().SetRight(new BinNode<char>('J')); // Dot for 'J'
-        root.GetRight().GetLeft().GetLeft().SetLeft(new BinNode<char>('B')); // Dash for 'B'
-        root.GetRight().GetLeft().GetLeft().SetRight(new BinNode<char>('X')); // Dash for 'X'
-        root.GetRight().GetRight().GetLeft().SetLeft(new BinNode<char>('C')); // Dash for 'C'
-        root.GetRight().GetRight().GetLeft().SetRight(new BinNode<char>('Y')); // Dash for 'Y'
-
-        return root;
+        return Root;
     }
+
 
     // Demo main function to run and test the decoding.
     public static void DemoMain()
@@ -129,19 +173,19 @@ public class RunTreeLs2
             Console.WriteLine($"Error: {ex.Message}");
         }
 
-        var root = new BinNode<int>(10);
-        var leftChild = new BinNode<int>(5);
-        var rightChild = new BinNode<int>(15);
-        root.SetLeft(leftChild);
-        root.SetRight(rightChild);
-        leftChild.SetLeft(new BinNode<int>(3));
-        leftChild.SetRight(new BinNode<int>(7));
-        rightChild.SetLeft(new BinNode<int>(12));
-        rightChild.SetRight(new BinNode<int>(18));
+        BinNode<int> root = new BinNode<int>(1);
+        root.SetLeft(new BinNode<int>(2));
+        root.SetRight(new BinNode<int>(3));
+        root.GetLeft().SetLeft(new BinNode<int>(4));
+        root.GetLeft().SetRight(new BinNode<int>(5));
+        root.GetRight().SetLeft(new BinNode<int>(6));
+        root.GetRight().SetRight(new BinNode<int>(7));
+
 
 
         TreeLs1.PrintTree(root);
 
         Console.WriteLine($"is the tree flat? :=> {TreeLs2.IsFlatTree(root)}");
+        Console.WriteLine($"is the tree young? :=> {TreeLs2.IsYoungTree(root)}");
     }
 }
