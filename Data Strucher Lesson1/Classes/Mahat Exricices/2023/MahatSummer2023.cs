367848b967b065e2160bb3f4c4ad84d95f00a12d
shay shalom izhak <alonizhakalonizhak@gmail.com>
2025-02-10 18:06:07 +0200

    added a few new changes to the codebase

--- Data Strucher Lesson1/Classes/Mahat Exricices/2023/MahatSummer2023.cs
+++ Data Strucher Lesson1/Classes/Mahat Exricices/2023/MahatSummer2023.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,6 +35,85 @@ namespace Data_Strucher_Lesson1.Classes.Mahat_Exricices._2023
             }
 
             return testCount;
+
+            // the runtime function is O(N) Beacuse ther is N parts in the function
+        }
+
+        //EX2
+        public static void Balance(Node<int> chain)
+        {
+            if (chain == null) return;
+
+            int countZeros = 0, countPositives = 0, countNegatives = 0;
+            Node<int> pos = chain;
+
+            while (pos != null)
+            {
+                int currentValue = pos.GetValue();
+                if (currentValue < 0)
+                    countNegatives++;
+                else if (currentValue > 0)
+                    countPositives++;
+                else
+                    countZeros++;
+
+                pos = pos.GetNext();
+            }
+
+            if (countPositives > 0 && countNegatives > 0 && countZeros > 0)
+            {
+                Console.WriteLine("The list is Balanced!");
+                return;
+            }
+
+            Random rnd = new Random();
+            Node<int> current = chain;
+
+            while (current.GetNext() != null)
+            {
+                current = current.GetNext();
+            }
+
+            if (countZeros == 0)
+            {
+                current.SetNext(new Node<int>(0));
+                current = current.GetNext();
+            }
+            if (countPositives == 0)
+            {
+                current.SetNext(new Node<int>(rnd.Next(1, 100)));
+                current = current.GetNext();
+            }
+            if (countNegatives == 0)
+            {
+                current.SetNext(new Node<int>(rnd.Next(-100, 0)));
+            }
+
+            // O(N) 
+            //O(N) הסבר: במקרה הגרוע ביותר, עלינו לעבור על כל הרשימה כדי להגיע לקשר האחרון ולהוסיף אליו אפס או מספר חיובי או שלילי. לכן, במקרה הגרוע, זמן הריצה הוא 
+
+        }
+
+        //EX5
+        public static bool IsSuper(Node<int> n)
+        {
+            Node<int> prev = n;
+            Node<int> current = n.GetNext();
+            Node<int> next = n.GetNext().GetNext();
+
+            while (prev.HasNext())
+            {
+                if (prev.GetValue() + current.GetValue()
+                   >= next.GetValue())
+                {
+                    return false;
+                }
+                prev = current;
+                current = next;
+                next = next.GetNext();
+
+            }
+            return true;
         }
     }
 
@@ -44,10 +124,16 @@ namespace Data_Strucher_Lesson1.Classes.Mahat_Exricices._2023
             Queue<int> marks = new Queue<int>();
             int[] scores = { 80, 90, 100, -1, 75, 96, -1, 100, 100, 97, 96, -1, -1, 88, 94, -1 };
 
+
+
             foreach (int score in scores)
             {
                 marks.Insert(score);
             }
+
+            Node<int> head = new Node<int>(5);
+            head.SetNext(new Node<int>(0));
+            head.GetNext().SetNext(new Node<int>(2));
 
             // Call ArrangeData function
             Console.ForegroundColor = ConsoleColor.Red;
@@ -58,6 +144,10 @@ namespace Data_Strucher_Lesson1.Classes.Mahat_Exricices._2023
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(marks);
             Console.ResetColor();
+            Console.WriteLine("==========");
+            Console.WriteLine(head.ToPrint());
+            MahatSummer2023.Balance(head);
+            Console.WriteLine(head.ToPrint());
 
         }
     }
