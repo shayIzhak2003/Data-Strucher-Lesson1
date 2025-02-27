c06ae08ed1ec520328be38b485b5be82e2285e7f
shay shalom izhak <alonizhakalonizhak@gmail.com>
2025-02-27 10:55:00 +0200

    added a few new changes to the codebase

---
+++ Data Strucher Lesson1/Classes/Mahat Exricices/2022/summer mohed a/EX10/SpeedCamera.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Data_Strucher_Lesson1.Classes.Mahat_Exricices._2022.summer_mohed_a.EX10
+{
+    public class SpeedCamera
+    {
+        private int cameraID;         // Unique ID for the speed camera
+        private int location;         // Location ID
+        private int speedLimit;       // Maximum allowed speed
+        private Stack<int> violations; // Stack of car speeds exceeding the limit
+
+        //Getters
+        //pt.1
+        public int GetCameraID() => this.cameraID;
+        public int GetLocation() => this.location;
+        public int GetSpeedLimit() => this.speedLimit;
+        public Stack<int> GetViolationsStack()
+        {
+            Stack<int> stack = new Stack<int>();
+            Stack<int> clone = new Stack<int>();
+
+            while (!this.violations.IsEmpty())
+            {
+                stack.Push(this.violations.Pop());
+            }
+            while (!stack.IsEmpty())
+            {
+                this.violations.Push(stack.Pop());
+                clone.Push(stack.Pop());
+            }
+            return clone;
+        }
+
+
+        //pt.2
+        public void AddCar(int carSpeed, int carNum)
+        {
+            if (carSpeed > this.speedLimit)
+            {
+                violations.Push(carNum);
+            }
+        }
+
+        public int GetVauletionsNum()
+        {
+            int count = 0;
+            Stack<int> temp = new Stack<int>();
+            while (!this.violations.IsEmpty())
+            {
+                temp.Push(this.violations.Pop());
+                count++;
+            }
+
+            while(!temp.IsEmpty())
+                this.violations.Push(temp.Pop());
+
+            return count;
+        }
+    }
+}
---
+++ Data Strucher Lesson1/Classes/Mahat Exricices/2022/summer mohed a/EX10/SpeedCameraNetwork.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Strucher_Lesson1.Classes.Mahat_Exricices._2022.summer_mohed_a.EX10
+{
+    public class SpeedCameraNetwork
+    {
+        private Stack<SpeedCamera> cameras; // Stack of speed cameras
+
+        public SpeedCameraNetwork()
+        {
+            cameras = new Stack<SpeedCamera>();
+        }
+
+        public void AddSpeedCamera(SpeedCamera sc)
+        {
+            cameras.Push(sc);
+        }
+
+        public int CountExtremeViolations()
+        {
+            int count = 0;
+            Stack<SpeedCamera> temp = new Stack<SpeedCamera>();
+
+            while (!this.cameras.IsEmpty())
+            {
+                SpeedCamera currentSc = cameras.Pop();
+                temp.Push(currentSc);
+
+                if (currentSc.GetVauletionsNum() > 200)
+                {
+                    count++;
+                }
+            }
+            while (!temp.IsEmpty())
+                this.cameras.Push(temp.Pop());
+
+            return count;
+        }
+
+        public bool CheckCarNumForViolations(int carNum)
+        {
+            Stack<SpeedCamera> temp = new Stack<SpeedCamera>();
+            bool flag = false;
+            while (!this.cameras.IsEmpty())
+            {
+                SpeedCamera currentSc = cameras.Pop();
+                while(!currentSc.GetViolationsStack().IsEmpty())
+                {
+                    int currentValue = currentSc.GetViolationsStack().Pop();
+                    if (carNum == currentValue)
+                    {
+                        Console.WriteLine(currentSc.GetCameraID());
+                        flag = true;
+                    }
+                }
+                temp.Push(currentSc);
+            }
+            while (!temp.IsEmpty())
+                this.cameras.Push(temp.Pop());
+
+            return flag;
+        }
+    }
+}
