c3b8c11c57fcccc95974f8c4631c47dfc5525261
shay shalom izhak <alonizhakalonizhak@gmail.com>
2025-06-24 20:48:11 +0300

    added a few new changes

--- Data Strucher Lesson1/Classes/Guy/NodeExGuy.cs
+++ Data Strucher Lesson1/Classes/Guy/NodeExGuy.cs
@@ -52,17 +52,41 @@ namespace Data_Strucher_Lesson1.Classes.Guy
             }
             return minP;
         }
+        // 
+        public static Node<int> AfterSteps(Node<int> lst, int steps, int x)
+        {
+            if (lst == null || steps < 0)
+                return lst;
+
+            Node<int> pos = lst;
+            int count = 0;
+            while (pos != null)
+            {
+                count++;
+                if (count == steps)
+                {
+                    Node<int> newValue = new Node<int>(x); // זוהי הדרך להוספת חוליה (תמיד!!)
+                    newValue.SetNext(pos.GetNext());
+                    pos.SetNext(newValue);
+                }
+                
+                pos = pos.GetNext();
+            }
+            return lst;
+        }
 
         public static void VirtualMain()
         {
-            Node<int> node5 = new Node<int>(14);
-            Node<int> node4 = new Node<int>(13, node5);
-            Node<int> node3 = new Node<int>(12, node4);
-            Node<int> node2 = new Node<int>(8, node3);
-            Node<int> node1 = new Node<int>(10, node2);
+            Node<int> node5 = new Node<int>(5);
+            Node<int> node4 = new Node<int>(4, node5);
+            Node<int> node3 = new Node<int>(3, node4);
+            Node<int> node2 = new Node<int>(2, node3);
+            Node<int> node1 = new Node<int>(1, node2);
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(MinNode(node1));
-            Console.ResetColor();
+            //Console.WriteLine(MinNode(node1));
+            Console.WriteLine(AfterSteps(node1, 2, 14));
+            Console.WriteLine(node1.ToPrint());
+
         }
     }
 }
